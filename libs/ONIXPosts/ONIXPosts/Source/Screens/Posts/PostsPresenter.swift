3960493098b33a1aa28116c219c95ec814c82afb
Jonathan Macías <jonathanmacias@MacBook-Pro-de-Jonathan.local>
2022-10-31 01:05:41 -0500

    Se implementó el loading del tableview de los posts

--- libs/ONIXPosts/ONIXPosts/Source/Screens/Posts/PostsPresenter.swift
+++ libs/ONIXPosts/ONIXPosts/Source/Screens/Posts/PostsPresenter.swift
@@ -13,6 +13,7 @@ protocol PostPresenterProtocol {
     func viewDidLoad()
     func getPostByIndex(index: Int) -> Post?
     var postCount: Int { get }
+    func reloadPosts(page: Int)
 }
 
 class PostsPresenter {
@@ -22,6 +23,7 @@ class PostsPresenter {
     var interactor: PostsInteractorProtocol
     var router: PostsRouterProtocol
     var posts: [Post] = []
+    var isLoading = false
     
     init(view: PostsViewProtocol, interactor: PostsInteractorProtocol, router: PostsRouterProtocol) {
         self.view = view
@@ -41,11 +43,19 @@ extension PostsPresenter: PostPresenterProtocol {
     }
     
     func viewDidLoad() {
-        fetchPosts(page: 100)
+        reloadPosts(page: 100)
+    }
+    
+    func reloadPosts(page: Int) {
+        isLoading = true
+        fetchPosts(page: page)
     }
     
     func fetchPosts(page: Int) {
+        view.loaderPosts(show: isLoading)
         interactor.fetchPosts(page: page) { result in
+            self.isLoading = false
+            self.view.loaderPosts(show: self.isLoading)
             switch result {
             case .success(let response):
                 if let data = response.data, let posts = data.posts, !posts.isEmpty {
--- libs/ONIXPosts/ONIXPosts/Source/Screens/Posts/PostsViewController.swift
+++ libs/ONIXPosts/ONIXPosts/Source/Screens/Posts/PostsViewController.swift
@@ -11,12 +11,15 @@ import ONIXEntites
 
 protocol PostsViewProtocol: AnyObject {
     func postsRetrivel(posts: [Post])
+    func loaderPosts(show: Bool)
 }
 
 public class PostsViewController: UIViewController {
     
     // MARK: - OULETS
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     
     // MARK: - PROPIEDADES
     var presenter: PostsPresenter!
@@ -40,13 +43,32 @@ public class PostsViewController: UIViewController {
         tableView.register(PostTableViewCell.getNib(), forCellReuseIdentifier: PostTableViewCell.cellId)
     }
     
+    func createActivityIndicatorFooter() -> UIView {
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 50))
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.center = footerView.center
+        footerView.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        return footerView
+    }
 
 }
 
 extension PostsViewController: PostsViewProtocol {
+    func loaderPosts(show: Bool) {
+        if show {
+            tableView.tableFooterView = createActivityIndicatorFooter()
+        } else {
+            DispatchQueue.main.async {
+                self.tableView.tableFooterView = nil
+            }
+        }
+    }
+    
     func postsRetrivel(posts: [ONIXEntites.Post]) {
         presenter.posts = posts
         DispatchQueue.main.async {
+            self.tableView.tableFooterView = nil
             self.tableView.reloadData()
         }
     }
@@ -65,6 +87,16 @@ extension PostsViewController: UITableViewDelegate, UITableViewDataSource {
         }
         cell.setupCell(postName: post.data?.title ?? "", postDescription: post.data?.description ?? "", authorName: post.data?.authorFullname ?? "", urlImage: post.data?.urlImage ?? "", commentsCount: (post.data?.commentsCount ?? 0).description)
         return cell
+    }
+    
+    // IDENTIFICAR CUANDO LA TABLA LLEGÓ AL FINAL PARA SOLICITAR MÁS POSTS
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let position = scrollView.contentOffset.y
+        let positionTable = ((self.tableView.contentSize.height-50) - scrollView.frame.size.height)
+        print("\(position) -- \(positionTable) -- \(scrollView.frame.size.height) -- \((self.tableView.contentSize.height-50))")
+        if position > positionTable, !presenter.isLoading {
+            presenter.reloadPosts(page: 100)
+        }
     }
     
     
